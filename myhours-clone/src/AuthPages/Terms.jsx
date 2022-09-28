81cf5e703b3be4c2c08653f946720aee73126258
pushpraj15295 <101568029+pushpraj15295@users.noreply.github.com>
2022-09-28 10:16:10 +0530

    signup-in && term-pages UI

---
+++ myhours-clone/src/AuthPages/Terms.jsx
@@ -0,0 +1,294 @@
+import { Box, Flex, Stack, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Terms = () => {
+  return (
+    <div style={{ backgroundColor: "white", color: "black" }}>
+      <Flex>
+        <Box width="80%" margin="auto" float="left">
+          <Text fontWeight="extrabold" fontSize="50px">
+            Terms of Use
+          </Text>
+          <br />
+          <br />
+          <Text fontWeight="bold" fontSize="35px">
+            GENERAL
+          </Text>
+          <br />
+          <Text fontSize="19px">
+            The My Hours service is an online,subscription-based, hosted,
+            supported and operated on- demand solution (“Cloud Service”)
+            provided by Špica International d.o.o. (“SPICA”) under a written
+            purchase order. SPICA is who is also the owner of My Hours website
+            (www.myhours.com),its contents and the software application. The
+            acceptance of this Terms of Use constitutes the written agreement
+            which is legally binding for both parties and valid until it is
+            terminated by one of the parties. SPICA is reserving the right to
+            amend or change the terms of use without prior notice. SPICA will
+            however take reasonable steps to inform users about any such change
+            and provide the easy-exit procedure in case of users wishing to
+            terminate the agreement and leave the service at any time, for any
+            reason. SPICA is not responsible for the Cloud Service
+            unavailability, data errors, inaccuracies or for any consequential
+            damage caused by the use, or inability to use the Cloud Service.
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            SPICA grants to users a non-exclusive,non-transferable and
+            world-wide right to use the Cloud Service (including its
+            implementation and configuration), any materials and documentation
+            solely for user’s internal business operations. Cloud Service may
+            include integrations with web services made available by third
+            parties(other than SPICA or its affiliates) that are accessed
+            through the Cloud Service and subject to terms and conditions with
+            those third parties. These third party’s web services are not part
+            of the Cloud Service and the agreement does not apply to them.
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            Support of the Cloud Service is performed in accordance with general
+            terms and conditions of support services.
+          </Text>
+          <Text fontWeight="bold" fontSize="35px">
+            PERSONAL INFORMATION AND PRIVACY
+          </Text>
+          <br />
+          <Text fontSize="19px">
+            My Hours collects personal information from its users and as a
+            service, allows users to collect personal information of other
+            users. According to EU regulation regarding personal data (GDPR)
+            Spica is the processor of the personal data managed by My Hours and
+            the User is deemed the controller. Furthermore, these Terms of Use
+            are deemed the Data Processing Agreement required by the regulation.
+            In case the User requires the agreement in a classic paper format
+            signed and dated by both parties, the signed PDF version will be
+            provided on request.
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            SPICA is committed to fulfill any request made by the Users stemming
+            from the provisions of personal data protection legislation, upon
+            user’s reasonable request. SPICA may charge the user reasonable
+            costs of fulfilling such requests.
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            Personal data is governed by My Hours Data Processing Agreement.
+          </Text>
+          <Text fontWeight="bold" fontSize="35px">
+            DATA SECURITY AND SAFETY
+          </Text>
+          <br />
+          <Text fontSize="19px">
+            SPICA is ISO27001 certified and takes all reasonable measures to
+            protect the information kept by My Hours from unauthorized use and
+            to prevent unauthorized access to My Hours and its data. My Hours
+            allows its administrative users (admins) to store, process and
+            manage personal information of other users such as employees,
+            temporarily hired workforce, subcontractors and other contributors
+            of their time tracking information. Admins are responsible for
+            protecting the privacy of the personal data they collect and manage
+            with the service. SPICA follows best business practices for
+            protecting the user data such as secure communication, data
+            encryption, user identification and authorization, rights
+            management, safe storage and redundancy. All the user information is
+            stored on servers in a secure operating environment. The My Hours
+            server software runs in a multi-tenant environment using logical
+            tenant isolation in Microsoft Azzure infrastructure. Although SPICA
+            has taken all reasonable measures to minimize the risks of data
+            loss, including the regular data backup, SPICA takes no
+            responsibility for eventual data recovery failure or data loss in
+            general. To help preserving the data in case of corruption, loss or
+            Cloud Service cancellation SPICA allows unhindered export of data
+            during the time of active subscription. SPICA is committed to
+            promptly inform the users about all security, privacy and data
+            safety incidents.{" "}
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            User is responsible for its data and entering it into the Cloud
+            Service. User grants to SPICA a nonexclusive right to process such
+            data solely to provide and support the Cloud Service.{" "}
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            At the end of the agreement, SPICA will delete the user’s data
+            remaining on servers hosting the Cloud Service unless applicable law
+            requires retention.
+          </Text>
+          <Text fontWeight="bold" fontSize="35px">
+            SUBSCRIPTION TERMS
+          </Text>
+          <br />
+          <Text fontSize="19px">
+            The Cloud service is paid by monthly or annual subscription. The
+            subscription is billed at the beginning of the subscription period.
+            Subscription is billed automatically according to the current number
+            of users. The customer account is billed according to the price
+            listed, the currency available for the country of the user’s origin
+            and the exchange rate used by the checkout system. The subscription
+            can be cancelled at any time by following the Settings link in the
+            global navigation bar. The Account screen provides a simple and
+            straight, no questions asked cancellation process. The customer is
+            solely responsible for properly cancelling his account. Email
+            messages are not accepted as cancellation. Spica, in its sole
+            discretion, reserves the right to suspend or terminate any customer
+            account and refuse any and all current or future use of the Cloud
+            service, or any other SPICA service, for any reason at anytime.{" "}
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            With respect to the Cloud service, user will not: except to the
+            extent such rights cannot be validly waived by law, disassemble,
+            decompile, reverse-engineer, copy, translate or make derivative
+            works, upload any content or data that is unlawful or infringes any
+            intellectual property rights, or circumvent or endanger its
+            operation or security.{" "}
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            SPICA may create analyses utilizing, in part, information derived
+            from user’s use of the Cloud service. Analyses will anonymize and
+            aggregate information, and will be treated as confidential
+            information. Examples of how analyses may be used include:optimizing
+            resources and support; research and development; automated processes
+            that enable continuous improvement, performance optimization and
+            development of new SPICA products and services; verification of
+            security and data integrity; internal demand planning; and data
+            products such as industry trends and developments, indices and
+            anonymous benchmarking.
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            SPICA warrants that during an applicable subscription term (a) this
+            agreement, and any documentation will accurately describe the
+            applicable administrative, physical,and technical safeguards for
+            protection of the security, confidentiality and integrity of user’s
+            data, (b) SPICA will not materially decrease the overall security of
+            the Cloud Services, (c) the Cloud Services will perform materially
+            in accordance with the applicable documentation, (d) will not breach
+            registered intellectual property rights of any 3rd party.
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            For any breach of a warranty above, user’s exclusive remedy is
+            termination.{" "}
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            {" "}
+            Any liability or warranty as regards the free usage of Cloud Service
+            being free of any material defects and defects in title in excess
+            thereof shall be precluded.
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            SPICA shall not be held liable for any loss,including loss of
+            profits, indirect or incidental loss, loss of data, non-function of
+            the Cloud Service or its functionalities if:{" "}
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            loss is not reproducible or not imputable to SPICA or in cases where
+            the Cloud Service is not used in compliance with this Agreement;
+            user has failed to properly perform its duty to collaborate or
+            failure to take the advice of SPICA; if the loss is caused as
+            consequence of the force majeure.{" "}
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            {" "}
+            SPICA’s total liability for any damages or claims under this
+            agreement is limited to the 10% of the value of the subscription
+            fees paid in the last 6 months, except in case SPICA caused the
+            damage with willful intent or gross negligence.
+          </Text>
+          <Text fontWeight="bold" fontSize="35px">
+            NON-PAYMENT AND DORMANT MODE
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            {" "}
+            Payment terms are 8 days from the date of the invoice . If My Hours
+            subscription is not paid one month after becoming due, the Cloud
+            service falls into dormant mode. In dormant mode, real-time time
+            tracking is still recorded. Dormant accounts are kept in dormant
+            mode for 90 days. During that time, the account can be reactivated
+            simply by paying one-month subscription, where the rate is
+            calculated according to the last month of active use. After 90 days,
+            dormant accounts are automatically cancelled.{" "}
+          </Text>
+          <Text fontWeight="bold" fontSize="35px">
+            TAX NOTES
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            {" "}
+            All the fees are exclusive of all taxes, levies, or duties imposed
+            by taxing authorities.Customers outside EU are responsible for
+            payment of all such taxes, levies, or duties. Customers within EU
+            are treated according to EU tax regulation, as follows:{" "}
+          </Text>
+          <Text fontSize="19px">
+            {" "}
+            Business customers from the EU are not charged Slovenian VAT, but
+            are required to submit their EU VAT registration number.
+          </Text>
+          <Text fontSize="19px">
+            {" "}
+            Non-business customers from the EU are charged additional 22% of
+            Slovenian VAT.{" "}
+          </Text>
+          <Text fontSize="19px">
+            All business and non-business customers from Slovenia are charged
+            additional 22% of Slovenian VAT.{" "}
+          </Text>
+          <Text fontWeight="bold" fontSize="35px">
+            GOVERNING LAW
+          </Text>
+          <br />{" "}
+          <Text fontSize="19px">
+            {" "}
+            The Agreement and any claims relating to its subject matter will be
+            governed by and construed under the laws of Slovenia, without
+            reference to its conflicts of law principles. All disputes will be
+            subject to the exclusive jurisdiction of the courts located in
+            Ljubljana. The United Nations Convention on Contracts for the
+            International Sale of Goods and the Uniform Computer Information
+            Transactions Act (where enacted) will not apply to the Agreement.{" "}
+          </Text>
+          <Text fontSize="19px">
+            {" "}
+            Business customers from the EU are not charged Slovenian VAT, but
+            are required to submit their EU VAT registration number.
+          </Text>
+          <br />
+          <br />
+          <Text fontWeight="bold" fontSize="35px">
+            ‍VALIDITY
+          </Text>
+          <br /> <Text fontSize="19px">Valid from: 14.2.2020 </Text>
+          <Text fontSize="19px">
+            {" "}
+            This version supersedes all previous versions.
+          </Text>
+          <br />
+          <br />
+          <Text fontWeight="bold" fontSize="35px">
+            CONTACT INFORMATION
+          </Text>
+          <br /> <Text fontSize="19px">Špica International d.o.o. </Text>
+          <Text fontSize="19px">Pot k sejmišču 33, 1000 Ljubljana</Text>
+          <Text fontSize="19px">Slovenija, EU</Text>
+          <Text fontSize="19px">www.spica.com</Text>
+          <br />
+          <br />
+          <br />
+          <br />
+        </Box>
+      </Flex>
+    </div>
+  );
+};
+
+export default Terms;
