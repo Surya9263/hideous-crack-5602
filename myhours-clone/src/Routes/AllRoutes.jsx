aa93ab7fa61857803bb9b67d7f3790bede7b396d
Surya Srivastava <suryasrivastava724@gmail.com>
2022-09-29 23:01:30 +0530

    use cases page added

--- myhours-clone/src/Routes/AllRoutes.jsx
+++ myhours-clone/src/Routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import LandingPage from '../components/Landing_Page/LandingPage'
 import Pricing from '../components/Pricing_page/Pricing'
+import UseCasePage from '../components/useCasePage/UseCasePage'
 import Support from '../components/utkarsh/Support'
 import Login from '../pages/AuthPages/SignIn'
 import HowItWorks from '../pages/HowItWorks'
@@ -10,10 +11,11 @@ const AllRoutes = () => {
     <div>
         <Routes>
             <Route path="/" element={<LandingPage/>} />
-            <Route path="/" element={<HowItWorks/>} />
-            <Route path="/" element={<Pricing/>} />
-            <Route path="/" element={<Support/>} />
-            <Route path="/" element={<Login/>} />
+            <Route path="/how-it-works" element={<HowItWorks/>} />
+            <Route path="/use-cases" element={<UseCasePage/>} />
+            <Route path="/pricing" element={<Pricing/>} />
+            <Route path="/support" element={<Support/>} />
+            <Route path="/signin" element={<Login/>} />
         </Routes>
     </div>
   )
