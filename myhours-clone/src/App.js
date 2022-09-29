b978966de111c7e0ad52c55385a97e38713c4c55
utkarshqw <utkarshup75@gmail.com>
2022-09-29 09:58:13 +0530

    navbar footer

--- myhours-clone/src/App.js
+++ myhours-clone/src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 // import Pricing from './Pricing_page/Pricing';
 import Navbar from './Component/utkarsh/Navbar';
 import Footer from './Component/utkarsh/Footer';
+import Support from './Component/utkarsh/Support';
 function App() {
   return (
     <div className="App">
       <Navbar/>
-    
+       {/* <Support/> */}
       <Footer/>
       
     
