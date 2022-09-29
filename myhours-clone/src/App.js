4f84c5ad8e69d2558047f2d08bc2a0a1727a8999
pushpraj15295 <101568029+pushpraj15295@users.noreply.github.com>
2022-09-29 10:51:00 +0530

    c

--- myhours-clone/src/App.js
+++ myhours-clone/src/App.js
@@ -1,22 +1,7 @@
 
-import Pricing from './components/Pricing_page/Pricing';
-import Login from './AuthPages/SignIn';
-import Signup from './AuthPages/SignUp';
-import Terms from './AuthPages/Terms';
-import Success from './AuthPages/Success';
-import LandingPage from './components/Landing_Page/LandingPage';
-import HowItWorks from "./pages/HowItWorks";
-
 function App() {
   return (
     <div>
-      <LandingPage/>
-     {/* <Pricing/> */}
-     {/* <Login/>
-     <Signup/>
-     <Terms/> */}
-     {/* <Success/> */}
-     {/* <HowItWorks /> */}
     </div>
   );
 }
