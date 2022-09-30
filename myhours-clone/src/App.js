13e0b4d691f72cc23a736686f21285e58492d860
unknown <shridharhakke1998@gmail.com>
2022-09-30 11:39:35 +0530

    updated

--- myhours-clone/src/App.js
+++ myhours-clone/src/App.js
@@ -1,12 +1,11 @@
 
-<<<<<<< HEAD
 import Pricing from './components/Pricing_page/Pricing';
-import Login from './AuthPages/SignIn';
-import Signup from './AuthPages/SignUp';
-import Terms from './AuthPages/Terms';
-import Success from './AuthPages/Success';
-import LandingPage from './components/Landing_Page/LandingPage';
-import HowItWorks from "./pages/HowItWorks";
+// import Login from './AuthPages/SignIn';
+// import Signup from './AuthPages/SignUp';
+// import Terms from './AuthPages/Terms';
+// import Success from './AuthPages/Success';
+// import LandingPage from './components/Landing_Page/LandingPage';
+// import HowItWorks from "./pages/HowItWorks";
 import Track_page from './components/Track_page/Track_page';
 
 
@@ -22,12 +21,6 @@ function App() {
      {/* <HowItWorks /> */}
   
   <Track_page/>
-=======
-
-function App() {
-  return (
-    <div>  
->>>>>>> 2b0d2b8749153b62e3aa9924cb6a2bdaff1d7a78
     </div>
   );
 }
