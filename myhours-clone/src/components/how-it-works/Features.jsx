07cb589df4acb0ee3c26aff8aae0911c0f43b367
pushpraj15295 <pushprajnda007@gmail.com>
2023-12-11 14:16:33 +0530

    useCase  page otimize and correct

--- myhours-clone/src/components/how-it-works/Features.jsx
+++ myhours-clone/src/components/how-it-works/Features.jsx
@@ -1,18 +1,21 @@
-import { Box, Heading, Img, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Heading, Img, Text, useMediaQuery } from "@chakra-ui/react";
+import React from "react";
 
 const Features = ({ title, description, featuresImg }) => {
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:800px)");
   return (
     <>
-    <Box>
-	<Box p={10} mt={20}>
-	<Heading fontSize={'30px'}>{title}</Heading>
-	<Text mt={6} fontSize='20px'>{description}</Text>
-	</Box>
-	<Img src={featuresImg} alt=''/>
-    </Box>
+      <Box>
+        <Box p={10} mt={isNotSmallerScreen ? "20" : "0"}>
+          <Heading fontSize={"30px"}>{title}</Heading>
+          <Text mt={6} fontSize="20px">
+            {description}
+          </Text>
+        </Box>
+        <Img src={featuresImg} alt={featuresImg} />
+      </Box>
     </>
-  )
-}
+  );
+};
 
 export default Features;
--- myhours-clone/src/utils/index.js
+++ myhours-clone/src/utils/index.js
@@ -24,3 +24,113 @@ export const landingImageData = [
         imgSrc:"https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/6000079d14844ef7226bb10a_logo_heading.png"
     },
 ]
+
+
+export const featuresData = [
+	{
+		title: "Organize your work with projects and tasks",
+		description: "Breaking down your work into projects and tasks is done with a few keystrokes. Prioritize tasks, add details and assign them to your team to complete. Set up an estimated budget on a project or task level.",
+		featuresImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd7198fa6df4249877e6d5b_HIW-1(1).png"
+	},
+	{
+		title: "Set flexible billable rates and manage rate changes",
+		description: "Every client, project, task and team member can be billed with a custom rate, but with smart defaults. And, when rates change, you decide how it applies to your past work.",
+		featuresImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/60c3106b6d15b4476d545c47_Project%20billing.png"
+	},
+	{
+		title: "Track your time working with all the details",
+		description: "Tracking work time is more than a few fields. Add styled notes to your logs, tag with custom keywords, attach files and enter expenses. With keyboard shortcuts, you can do it in no-time.",
+		featuresImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/61693180d0c70441fdcc29cd_Timesheet%20with%20timer-p-1080.png"
+	},
+	{
+		title: "Dazzle your clients with custom reports",
+		description: "No more one-style-fits-all reports. Go beyond filters and set up reports the way you or your clients want them, without having to retreat back to Excel.",
+		featuresImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd71990c1a06b2e03f1be70_HIW-4(1).png"
+	},
+	{
+		title: "Look at the time spent from every angle",
+		description: "Fancy dashboards are not enough and with My Hours you can calculate your profit, generate real-time pivot tables or download a simple weekly timesheet.",
+		featuresImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/61692c9dbe4680c6916e960b_time-analytics.png"
+	},
+	{
+		title: "Approve or comment on your team’s time logs  ",
+		description: "Going through every timesheet is a hassle. Select a few projects or team members that might need a careful inspection, while other time logs do not need explicit approval.",
+		featuresImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/61692ce83988430ca662abb6_Approve%20timesheets-p-1080.png"
+	}
+];
+
+export const stepsData = [
+	{
+		title: "Keep clients in the loop with scheduled email reports",
+		description: "Setting and downloading reports each month takes precious time. Save your report settings and have My Hours send it to you or your clients directly every period.",
+		stepsImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc4e6bd377fbe34a0cc7a0f_HIW-7.png"
+	},
+	{
+		title: "Create invoices straight from your tracked hours",
+		description: "No need for an additional tool that handles the invoicing. Calculate the billable amount for each client and generate a numbered invoice that can be sent via email straight away.",
+		stepsImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc4e6d0c755c679d71eda9f_HIW-8.png"
+	},
+	{
+		title: "Flexible, but simple access roles",
+		description: "With a simple 3-role system, you’ll always know who sees what. You can hide costs and billable amounts from anyone, except account administrators.",
+		stepsImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc913df96a46f4b42ee4264_Roles-p-800.png"
+	},
+	{
+		title: "Count your chickens and calculate your costs",
+		description: "On top of billable rates, teams can add labor costs and expenses. Monitor your project costs with a (monthly) budget and calculate your profits or losses.",
+		stepsImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc91420b5951fed1bb2a187_Chickens(1).png"
+	},
+];
+
+export const SupportData = [
+	{
+		title: "Sync your data with more than a 1000 tools",
+		description: "Transfer clients, projects and tasks from Quickbooks and other solutions via Zapier. Documented API enables tech-savvy folks to build their own integrations.",
+		supportImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc9145d6e47e935f0570ff3_Integrations-p-500.png"
+	},
+	{
+		title: "Track time on the go with a mobile app",
+		description: "Besides tracking time, add new projects, manage tasks and view reports - all through our mobile apps. Both iOS and Android are supported.",
+		supportImg: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd73713f912e4498947d1a3_HIW-mobile%20(1)(1)-p-500.png"
+	},
+]
+
+
+export const useCaseData=[
+    {
+      id:1,
+      imgUrl:"https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fbf86f7f19477d80b3be5ec_Billable%20settings-p-500.png",
+      title:"Project billing",
+      dis:"Choose the billing method that is fair for your client and represents your actual work."
+    },
+     {
+      id:2,
+      imgUrl:"https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fbf86f7f19477d80b3be5ec_Billable%20settings-p-500.png",
+      title:"Time reports & Project analytics",
+      dis:"Always know how much time has been spent on your projects and tasks in a specific period of time with easy-to-create reports."
+    },
+     {
+      id:3,
+      imgUrl:"https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc4e6828a88f435b7c4ac2a_HIW-4-p-500.png",
+      title:"Attendance & Absence",
+      dis:" Know who was present at work and track absences and the reasons behind them. Make your payroll faster and more accurate by exporting all the tracked data. Time, attendance and absence tracking in one streamlined solution."
+    },
+     {
+      id:4,
+      imgUrl:"https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc91420b5951fed1bb2a187_Chickens(1).png",
+      title:"Expense tracking",
+      dis:"Get expenses of your mind and into your invoice, without losing a cent or a receipt."
+    },
+     {
+      id:5,
+      imgUrl:"https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc91420b5951fed1bb2a187_Chickens(1).png",
+      title:"Calculating project profitability",
+      dis:"Grow your business faster by prioritizing the projects that are worth your time, effort and resources."
+    },
+     {
+      id:6,
+      imgUrl:"https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd20dcf9d62a4f65fea8acd_Frame%2027(1)(1)-p-500.png",
+      title:"Timesheet time tracking",
+      dis:"Track time just as you are used to, but simpler Because you are tired of using unmanageable stacks of paper, excel templates, or complex apps week in week out."
+    },
+   ]
